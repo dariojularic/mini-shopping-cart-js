0759e3e5e81a80cf953cac100f81c3ddb24ccae5
Dario Jularic <dariojularic0@gmail.com>
2024-07-07 19:54:34 +0200

    Create classes

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
 import './style.css'
+
+class Item{
+  constructor(name, price, quantity) {
+    this.id = crypto.randomUUID()
+    this.name = name
+    this.price = price
+    this.quantity = quantity
+    this.isInStock = true
+  }
+
+  notInStock() {
+    this.isInStock = false
+  }
+}
+
+class ShopManager{
+  constructor() {
+    this.shop = []
+  }
+
+  addToShop(item) {
+    this.shop.push(item)
+  }
+
+  removeFromShop(item) {
+  
+  }
+}
+
+class CartManager{
+  constructor() {
+    this.cart = []
+  }
+
+  addToCart(item) {
+    this.cart.push(item)
+  }
+
+  removeFromCart(itemId) {
+
+  }
+}
